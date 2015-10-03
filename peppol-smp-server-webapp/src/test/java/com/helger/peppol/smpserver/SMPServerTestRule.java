ccc96e131eabe5fb674dd7fa71a8c798646d18f4
phax <philip@helger.com>
2015-10-03 10:55:52 +0200

    Init only once

--- peppol-smp-server-webapp/src/test/java/com/helger/peppol/smpserver/SMPServerTestRule.java
+++ peppol-smp-server-webapp/src/test/java/com/helger/peppol/smpserver/SMPServerTestRule.java
@@ -1,20 +1,27 @@
 package com.helger.peppol.smpserver;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import com.helger.peppol.smpserver.servlet.SMPWebAppListener;
 import com.helger.photon.basic.mock.PhotonBasicWebTestRule;
 
 /**
  * Special SMP server JUnit test rule.
- * 
+ *
  * @author Philip Helger
  */
 public class SMPServerTestRule extends PhotonBasicWebTestRule
 {
+  private static final AtomicBoolean s_aInitBackend = new AtomicBoolean (false);
+
   @Override
   public void before ()
   {
     super.before ();
-    SMPWebAppListener.initBackendFromConfiguration ();
+
+    // Set it only once
+    if (s_aInitBackend.compareAndSet (false, true))
+      SMPWebAppListener.initBackendFromConfiguration ();
   }
 
   @Override
