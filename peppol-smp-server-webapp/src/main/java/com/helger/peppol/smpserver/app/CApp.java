3fcb39523423b91716c76f5efdf0cd687b37163b
phax <philip@helger.com>
2015-11-16 21:55:22 +0100

    Updated to ph-oton latest

--- peppol-smp-server-webapp/src/main/java/com/helger/peppol/smpserver/app/CApp.java
+++ peppol-smp-server-webapp/src/main/java/com/helger/peppol/smpserver/app/CApp.java
@@ -45,11 +45,11 @@ public final class CApp
   public static final String ROLE_CONFIG_ID = "config";
   public static final String ROLE_CONFIG_NAME = "Config user";
   public static final String ROLE_CONFIG_DESCRIPTION = null;
-  public static final Map <String, ?> ROLE_CONFIG_CUSTOMATTRS = null;
+  public static final Map <String, String> ROLE_CONFIG_CUSTOMATTRS = null;
   public static final String ROLE_VIEW_ID = "view";
   public static final String ROLE_VIEW_NAME = "View user";
   public static final String ROLE_VIEW_DESCRIPTION = null;
-  public static final Map <String, ?> ROLE_VIEW_CUSTOMATTRS = null;
+  public static final Map <String, String> ROLE_VIEW_CUSTOMATTRS = null;
 
   public static final List <String> REQUIRED_ROLE_IDS_CONFIG = CollectionHelper.newUnmodifiableList (ROLE_CONFIG_ID);
   public static final List <String> REQUIRED_ROLE_IDS_VIEW = CollectionHelper.newUnmodifiableList (ROLE_VIEW_ID);
@@ -58,15 +58,15 @@ public final class CApp
   public static final String USERGROUP_ADMINISTRATORS_ID = CSecurity.USERGROUP_ADMINISTRATORS_ID;
   public static final String USERGROUP_ADMINISTRATORS_NAME = CSecurity.USERGROUP_ADMINISTRATORS_NAME;
   public static final String USERGROUP_ADMINISTRATORS_DESCRIPTION = null;
-  public static final Map <String, ?> USERGROUP_ADMINISTRATORS_CUSTOMATTRS = null;
+  public static final Map <String, String> USERGROUP_ADMINISTRATORS_CUSTOMATTRS = null;
   public static final String USERGROUP_CONFIG_ID = "ugconfig";
   public static final String USERGROUP_CONFIG_NAME = "Config user";
   public static final String USERGROUP_CONFIG_DESCRIPTION = null;
-  public static final Map <String, ?> USERGROUP_CONFIG_CUSTOMATTRS = null;
+  public static final Map <String, String> USERGROUP_CONFIG_CUSTOMATTRS = null;
   public static final String USERGROUP_VIEW_ID = "ugview";
   public static final String USERGROUP_VIEW_NAME = "View user";
   public static final String USERGROUP_VIEW_DESCRIPTION = null;
-  public static final Map <String, ?> USERGROUP_VIEW_CUSTOMATTRS = null;
+  public static final Map <String, String> USERGROUP_VIEW_CUSTOMATTRS = null;
 
   // User ID
   public static final String USER_ADMINISTRATOR_ID = CSecurity.USER_ADMINISTRATOR_ID;
@@ -77,7 +77,7 @@ public final class CApp
   public static final String USER_ADMINISTRATOR_LASTNAME = CSecurity.USER_ADMINISTRATOR_NAME;
   public static final String USER_ADMINISTRATOR_DESCRIPTION = null;
   public static final Locale USER_ADMINISTRATOR_LOCALE = CApp.DEFAULT_LOCALE;
-  public static final Map <String, ?> USER_ADMINISTRATOR_CUSTOMATTRS = null;
+  public static final Map <String, String> USER_ADMINISTRATOR_CUSTOMATTRS = null;
 
   private CApp ()
   {}
