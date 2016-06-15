3c8474ca94851bb6a5e9d6b22e289973aa2fa497
Philip Helger <philip@helger.com>
2016-06-15 16:51:17 +0200

    Made webapps.properties loading more flexible

--- peppol-smp-server-webapp/src/main/java/com/helger/peppol/smpserver/app/AppSettings.java
+++ peppol-smp-server-webapp/src/main/java/com/helger/peppol/smpserver/app/AppSettings.java
@@ -23,46 +23,52 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.helger.commons.annotation.UsedViaReflection;
+import com.helger.commons.collection.ext.CommonsArrayList;
+import com.helger.commons.collection.ext.ICommonsList;
 import com.helger.commons.debug.GlobalDebug;
-import com.helger.commons.io.resource.ClassPathResource;
-import com.helger.commons.io.resource.FileSystemResource;
 import com.helger.commons.io.resource.IReadableResource;
 import com.helger.commons.scope.singleton.AbstractGlobalSingleton;
 import com.helger.commons.string.StringHelper;
 import com.helger.commons.system.SystemProperties;
+import com.helger.peppol.utils.ConfigFile;
 import com.helger.settings.ISettings;
-import com.helger.settings.exchange.properties.SettingsPersistenceProperties;
 
 /**
  * This class provides access to the web application settings. If the system
- * property <code>smp.webapp.properties.path</code> is defined, the
- * configuration file is read from the absolute path stated there. Otherwise (by
- * default) the configuration settings contained in the
+ * property <code>peppol.smp.webapp.properties.path</code> or
+ * <code>smp.webapp.properties.path</code> is defined, the configuration file is
+ * read from the absolute path stated there. Otherwise (by default) the
+ * configuration settings contained in the
+ * <code>src/main/resources/private-webapp.properties</code> or
  * <code>src/main/resources/webapp.properties</code> file are read.
  *
  * @author Philip Helger
  */
 public final class AppSettings extends AbstractGlobalSingleton
 {
-  /** The name of the classpath resource containing the settings */
-  public static final String FILENAME = "webapp.properties";
-
   private static final Logger s_aLogger = LoggerFactory.getLogger (AppSettings.class);
 
-  private static final IReadableResource s_aRes;
-  private static final ISettings s_aSettings;
+  private static final ConfigFile s_aConfigFile;
 
   static
   {
-    final String sPropertyPath = SystemProperties.getPropertyValue ("smp.webapp.properties.path");
+    final ICommonsList <String> aFilePaths = new CommonsArrayList <> ();
+    // Check if the system property is present
+    String sPropertyPath = SystemProperties.getPropertyValue ("peppol.smp.webapp.properties.path");
     if (StringHelper.hasText (sPropertyPath))
-      s_aRes = new FileSystemResource (sPropertyPath);
-    else
-      s_aRes = new ClassPathResource (FILENAME);
+      aFilePaths.add (sPropertyPath);
+    sPropertyPath = SystemProperties.getPropertyValue ("smp.webapp.properties.path");
+    if (StringHelper.hasText (sPropertyPath))
+      aFilePaths.add (sPropertyPath);
 
-    s_aLogger.info ("Reading webapp.properties from " + s_aRes.getPath ());
-    final SettingsPersistenceProperties aSPP = new SettingsPersistenceProperties ();
-    s_aSettings = aSPP.readSettings (s_aRes);
+    // Use the default paths
+    aFilePaths.add ("private-webapp.properties");
+    aFilePaths.add ("webapp.properties");
+
+    s_aConfigFile = ConfigFile.create (aFilePaths);
+    if (!s_aConfigFile.isRead ())
+      throw new IllegalStateException ("Failed to read PEPPOL SMP UI properties from any of the paths: " + aFilePaths);
+    s_aLogger.info ("Read PEPPOL SMP UI properties from " + s_aConfigFile.getReadResource ().getPath ());
   }
 
   @Deprecated
@@ -73,40 +79,40 @@ public final class AppSettings extends AbstractGlobalSingleton
   @Nonnull
   public static ISettings getSettingsObject ()
   {
-    return s_aSettings;
+    return s_aConfigFile.getSettings ();
   }
 
   @Nonnull
   public static IReadableResource getSettingsResource ()
   {
-    return s_aRes;
+    return s_aConfigFile.getReadResource ();
   }
 
   @Nullable
   public static String getGlobalDebug ()
   {
-    return s_aSettings.getAsString ("global.debug");
+    return s_aConfigFile.getAsString ("global.debug");
   }
 
   @Nullable
   public static String getGlobalProduction ()
   {
-    return s_aSettings.getAsString ("global.production");
+    return s_aConfigFile.getAsString ("global.production");
   }
 
   @Nullable
   public static String getDataPath ()
   {
-    return s_aSettings.getAsString ("webapp.datapath");
+    return s_aConfigFile.getAsString ("webapp.datapath");
   }
 
   public static boolean isCheckFileAccess ()
   {
-    return s_aSettings.getAsBoolean ("webapp.checkfileaccess", true);
+    return s_aConfigFile.getAsBoolean ("webapp.checkfileaccess", true);
   }
 
   public static boolean isTestVersion ()
   {
-    return s_aSettings.getAsBoolean ("webapp.testversion", GlobalDebug.isDebugMode ());
+    return s_aConfigFile.getAsBoolean ("webapp.testversion", GlobalDebug.isDebugMode ());
   }
 }
