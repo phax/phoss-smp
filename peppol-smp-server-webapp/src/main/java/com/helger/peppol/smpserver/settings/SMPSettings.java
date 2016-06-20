c95778d320fe14a46b1587b5ec4ff142738b6e8b
phax <philip@helger.com>
2016-06-20 18:22:02 +0200

    Started serialization

--- peppol-smp-server-webapp/src/main/java/com/helger/peppol/smpserver/settings/SMPSettings.java
+++ peppol-smp-server-webapp/src/main/java/com/helger/peppol/smpserver/settings/SMPSettings.java
@@ -3,8 +3,12 @@ package com.helger.peppol.smpserver.settings;
 import javax.annotation.Nonnull;
 import javax.annotation.concurrent.NotThreadSafe;
 
+import com.helger.commons.ValueEnforcer;
+import com.helger.commons.annotation.ReturnsMutableCopy;
 import com.helger.commons.state.EChange;
 import com.helger.peppol.smpserver.SMPServerConfiguration;
+import com.helger.settings.ISettings;
+import com.helger.settings.Settings;
 
 /**
  * This class contains the settings to be applied for the current SMP instance.
@@ -33,5 +37,20 @@ public class SMPSettings implements ISMPSettings
       return EChange.UNCHANGED;
     m_bPEPPOLDirectoryIntegrationEnabled = bPEPPOLDirectoryIntegrationEnabled;
     return EChange.CHANGED;
+  }
+
+  @Nonnull
+  @ReturnsMutableCopy
+  public ISettings getAsSettings ()
+  {
+    final Settings ret = new Settings ("smp");
+    ret.setValue ("peppol.directory.integration.enabled", m_bPEPPOLDirectoryIntegrationEnabled);
+    return ret;
+  }
+
+  public void setFromSettings (@Nonnull final ISettings aSettings)
+  {
+    ValueEnforcer.notNull (aSettings, "settings");
+    m_bPEPPOLDirectoryIntegrationEnabled = aSettings.getAsBoolean ("peppol.directory.integration.enabled");
   }
 }
--- peppol-smp-server-webapp/src/main/java/com/helger/peppol/smpserver/settings/SMPSettingsManager.java
+++ peppol-smp-server-webapp/src/main/java/com/helger/peppol/smpserver/settings/SMPSettingsManager.java
@@ -1,34 +1,87 @@
 package com.helger.peppol.smpserver.settings;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import javax.annotation.concurrent.ThreadSafe;
 
+import com.helger.commons.annotation.UsedViaReflection;
 import com.helger.commons.concurrent.SimpleReadWriteLock;
+import com.helger.commons.scope.singleton.AbstractGlobalSingleton;
 import com.helger.commons.state.EChange;
+import com.helger.photon.basic.app.dao.impl.AbstractSimpleDAO;
+import com.helger.photon.basic.app.dao.impl.DAOException;
+import com.helger.settings.ISettings;
+import com.helger.settings.exchange.xml.SettingsMicroDocumentConverter;
+import com.helger.settings.factory.ISettingsFactory;
+import com.helger.xml.microdom.IMicroDocument;
+import com.helger.xml.microdom.MicroDocument;
 
 @ThreadSafe
-public class SMPSettingsManager
+public class SMPSettingsManager extends AbstractGlobalSingleton
 {
-  private static SimpleReadWriteLock s_aRWLock = new SimpleReadWriteLock ();
-  private static SMPSettings s_aSettings = new SMPSettings ();
-
-  public SMPSettingsManager ()
-  {}
-
-  @Nonnull
-  public static ISMPSettings getSettings ()
+  private final class DAO extends AbstractSimpleDAO
   {
-    return s_aSettings;
+    protected DAO (@Nullable final String sFilename) throws DAOException
+    {
+      super (sFilename);
+      initialRead ();
+    }
+
+    @Override
+    @Nonnull
+    protected EChange onRead (final IMicroDocument aDoc)
+    {
+      final SettingsMicroDocumentConverter aConverter = new SettingsMicroDocumentConverter (ISettingsFactory.newInstance ());
+      final ISettings aSettings = aConverter.convertToNative (aDoc.getDocumentElement ());
+      m_aSMPS.setFromSettings (aSettings);
+      return EChange.UNCHANGED;
+    }
+
+    @Override
+    protected IMicroDocument createWriteData ()
+    {
+      final IMicroDocument ret = new MicroDocument ();
+      final SettingsMicroDocumentConverter aConverter = new SettingsMicroDocumentConverter (ISettingsFactory.newInstance ());
+      ret.appendChild (aConverter.convertToMicroElement (m_aSMPS.getAsSettings (), null, "root"));
+      return ret;
+    }
+
+    private void markAsChanged0 ()
+    {
+      super.m_aRWLock.writeLocked ( () -> super.markAsChanged ());
+    }
   }
 
-  public static EChange updateSettings (final boolean bPEPPOLDirectoryIntegrationEnabled)
+  private final SimpleReadWriteLock m_aRWLock = new SimpleReadWriteLock ();
+  private final SMPSettings m_aSMPS = new SMPSettings ();
+  private final DAO m_aDAO;
+
+  @Deprecated
+  @UsedViaReflection
+  public SMPSettingsManager () throws DAOException
+  {
+    m_aDAO = new DAO ("smp-settings.xml");
+  }
+
+  @Nonnull
+  public static SMPSettingsManager getInstance ()
+  {
+    return getGlobalSingleton (SMPSettingsManager.class);
+  }
+
+  @Nonnull
+  public ISMPSettings getSettings ()
+  {
+    return m_aSMPS;
+  }
+
+  @Nonnull
+  public EChange updateSettings (final boolean bPEPPOLDirectoryIntegrationEnabled)
   {
     EChange eChange = EChange.UNCHANGED;
-    eChange = eChange.or (s_aSettings.setPEPPOLDirectoryIntegrationEnabled (bPEPPOLDirectoryIntegrationEnabled));
+    eChange = eChange.or (m_aSMPS.setPEPPOLDirectoryIntegrationEnabled (bPEPPOLDirectoryIntegrationEnabled));
     if (eChange.isChanged ())
-    {
-
-    }
+      m_aDAO.markAsChanged0 ();
     return eChange;
   }
 }
