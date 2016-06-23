d3eeb5ee607cdbfc9b38580654e8ab043fdbcab2
Philip Helger <philip@helger.com>
2016-06-23 16:35:20 +0200

    Placeholder class

---
+++ peppol-smp-server-webapp/src/main/java/com/helger/peppol/smpserver/ui/AbstractSMPWebPageSimpleForm.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (C) 2014-2016 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.peppol.smpserver.ui;
+
+import javax.annotation.Nonnull;
+
+import com.helger.commons.annotation.Nonempty;
+import com.helger.commons.id.IHasID;
+import com.helger.photon.bootstrap3.pages.AbstractBootstrapWebPageSimpleForm;
+import com.helger.photon.uicore.page.WebPageExecutionContext;
+
+public abstract class AbstractSMPWebPageSimpleForm <DATATYPE extends IHasID <String>> extends
+                                                   AbstractBootstrapWebPageSimpleForm <DATATYPE, WebPageExecutionContext>
+{
+  public AbstractSMPWebPageSimpleForm (@Nonnull @Nonempty final String sID, @Nonnull final String sName)
+  {
+    super (sID, sName);
+  }
+}
