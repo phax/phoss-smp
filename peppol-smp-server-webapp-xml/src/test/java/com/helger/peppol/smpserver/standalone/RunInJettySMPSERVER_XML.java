48b6fab4bc2ab8a9d5d85803a785be9c0e578abd
Philip Helger <philip@helger.com>
2017-06-08 18:50:09 +0200

    Apply system properties on startup

--- peppol-smp-server-webapp-xml/src/test/java/com/helger/peppol/smpserver/standalone/RunInJettySMPSERVER_XML.java
+++ peppol-smp-server-webapp-xml/src/test/java/com/helger/peppol/smpserver/standalone/RunInJettySMPSERVER_XML.java
@@ -16,6 +16,7 @@
  */
 package com.helger.peppol.smpserver.standalone;
 
+import com.helger.peppol.smpserver.SMPServerConfiguration;
 import com.helger.photon.jetty.JettyStarter;
 
 /**
@@ -32,6 +33,7 @@ public final class RunInJettySMPSERVER_XML
 {
   public static void main (final String... args) throws Exception
   {
+    SMPServerConfiguration.getConfigFile ().applyAllNetworkSystemProperties ();
     new JettyStarter (RunInJettySMPSERVER_XML.class).setPort (90).setStopPort (8078).run ();
   }
 }
