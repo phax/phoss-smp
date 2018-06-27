222a89086e9d90fde4a451c531cb205e98b4d583
Philip Helger <philip@helger.com>
2018-06-27 15:19:16 +0200

    Version update

--- peppol-smp-server-sql/src/main/java/com/helger/peppol/smpserver/data/sql/spi/SQLSMPBackendRegistrarSPI.java
+++ peppol-smp-server-sql/src/main/java/com/helger/peppol/smpserver/data/sql/spi/SQLSMPBackendRegistrarSPI.java
@@ -13,7 +13,6 @@ package com.helger.peppol.smpserver.data.sql.spi;
 import javax.annotation.Nonnull;
 
 import com.helger.commons.annotation.IsSPIImplementation;
-import com.helger.commons.factory.FactoryNewInstance;
 import com.helger.peppol.smpserver.backend.ISMPBackendRegistrarSPI;
 import com.helger.peppol.smpserver.backend.ISMPBackendRegistry;
 import com.helger.peppol.smpserver.data.sql.mgr.SQLManagerProvider;
@@ -28,6 +27,6 @@ public final class SQLSMPBackendRegistrarSPI implements ISMPBackendRegistrarSPI
 {
   public void registerSMPBackend (@Nonnull final ISMPBackendRegistry aRegistry)
   {
-    aRegistry.registerSMPBackend ("sql", FactoryNewInstance.create (SQLManagerProvider.class));
+    aRegistry.registerSMPBackend ("sql", SQLManagerProvider::new);
   }
 }
--- peppol-smp-server-xml/src/main/java/com/helger/peppol/smpserver/data/xml/spi/XMLSMPBackendRegistrarSPI.java
+++ peppol-smp-server-xml/src/main/java/com/helger/peppol/smpserver/data/xml/spi/XMLSMPBackendRegistrarSPI.java
@@ -19,7 +19,6 @@ package com.helger.peppol.smpserver.data.xml.spi;
 import javax.annotation.Nonnull;
 
 import com.helger.commons.annotation.IsSPIImplementation;
-import com.helger.commons.factory.FactoryNewInstance;
 import com.helger.peppol.smpserver.backend.ISMPBackendRegistrarSPI;
 import com.helger.peppol.smpserver.backend.ISMPBackendRegistry;
 import com.helger.peppol.smpserver.data.xml.mgr.XMLManagerProvider;
@@ -34,6 +33,6 @@ public final class XMLSMPBackendRegistrarSPI implements ISMPBackendRegistrarSPI
 {
   public void registerSMPBackend (@Nonnull final ISMPBackendRegistry aRegistry)
   {
-    aRegistry.registerSMPBackend ("xml", FactoryNewInstance.create (XMLManagerProvider.class));
+    aRegistry.registerSMPBackend ("xml", () -> new XMLManagerProvider ());
   }
 }
