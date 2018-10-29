994189bd1fcfe21a95c3b5944204ed894920151f
Philip Helger <philip@helger.com>
2018-10-29 06:17:27 +0100

    Updated to 5.0.7

--- docker/build-release-latest.cmd
+++ docker/build-release-latest.cmd
@@ -16,7 +16,7 @@
 @REM
 
 @echo off
-set XVER=5.0.6
+set XVER=5.0.7
 docker build --build-arg VERSION=%XVER% -t phoss-smp-release-binary-xml-%XVER% -f Dockerfile-release-binary-xml .
 if errorlevel 1 goto end
 docker tag phoss-smp-release-binary-xml-%XVER% phelger/smp:%XVER%
