dd2fe36b512105f6bc9f71198c94fd9c73e55533
Philip Helger <philip@helger.com>
2018-11-03 19:56:50 +0100

    License headers

--- peppol-smp-server-library/src/main/java/com/helger/peppol/smpserver/exception/SMPInternalErrorException.java
+++ peppol-smp-server-library/src/main/java/com/helger/peppol/smpserver/exception/SMPInternalErrorException.java
@@ -1,3 +1,13 @@
+/**
+ * Copyright (C) 2015-2018 Philip Helger and contributors
+ * philip[at]helger[dot]com
+ *
+ * The Original Code is Copyright The PEPPOL project (http://www.peppol.eu)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.helger.peppol.smpserver.exception;
 
 import javax.annotation.Nonnull;
