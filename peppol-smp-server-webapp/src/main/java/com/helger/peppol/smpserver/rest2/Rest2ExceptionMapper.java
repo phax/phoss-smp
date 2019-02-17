7f2cbeda8d3c9f039018d6fed91c7d7103b4042e
Philip Helger <philip@helger.com>
2019-02-17 21:06:49 +0100

    Extracted exception mapper into explicit class; updated license headers

---
+++ peppol-smp-server-webapp/src/main/java/com/helger/peppol/smpserver/rest2/Rest2ExceptionMapper.java
@@ -0,0 +1,136 @@
+/**
+ * Copyright (C) 2014-2019 Philip Helger and contributors
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.peppol.smpserver.rest2;
+
+import javax.annotation.Nonnull;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.helger.commons.debug.GlobalDebug;
+import com.helger.commons.state.EHandled;
+import com.helger.peppol.smpserver.SMPServerConfiguration;
+import com.helger.peppol.smpserver.exception.SMPBadRequestException;
+import com.helger.peppol.smpserver.exception.SMPInternalErrorException;
+import com.helger.peppol.smpserver.exception.SMPNotFoundException;
+import com.helger.peppol.smpserver.exception.SMPSMLException;
+import com.helger.peppol.smpserver.exception.SMPServerException;
+import com.helger.peppol.smpserver.exception.SMPUnauthorizedException;
+import com.helger.peppol.smpserver.exception.SMPUnknownUserException;
+import com.helger.photon.core.api.AbstractAPIExceptionMapper;
+import com.helger.photon.core.api.InvokableAPIDescriptor;
+import com.helger.servlet.response.UnifiedResponse;
+import com.helger.web.scope.IRequestWebScopeWithoutResponse;
+
+/**
+ * Special API exception mapper for the SMP REST API.
+ *
+ * @author Philip Helger
+ */
+public class Rest2ExceptionMapper extends AbstractAPIExceptionMapper
+{
+  private static final Logger LOGGER = LoggerFactory.getLogger (Rest2ExceptionMapper.class);
+
+  @Nonnull
+  public EHandled applyExceptionOnResponse (final InvokableAPIDescriptor aInvokableDescriptor,
+                                            final IRequestWebScopeWithoutResponse aRequestScope,
+                                            final UnifiedResponse aUnifiedResponse,
+                                            final Throwable aThrowable)
+  {
+    // From specific to general
+    if (aThrowable instanceof SMPUnauthorizedException)
+    {
+      if (SMPServerConfiguration.isRESTLogExceptions ())
+        LOGGER.error ("Unauthorized", aThrowable);
+      setSimpleTextResponse (aUnifiedResponse,
+                             HttpServletResponse.SC_FORBIDDEN,
+                             getResponseEntityWithoutStackTrace (aThrowable));
+      return EHandled.HANDLED;
+    }
+    if (aThrowable instanceof SMPUnknownUserException)
+    {
+      if (SMPServerConfiguration.isRESTLogExceptions ())
+        LOGGER.error ("Unknown user", aThrowable);
+      setSimpleTextResponse (aUnifiedResponse,
+                             HttpServletResponse.SC_FORBIDDEN,
+                             getResponseEntityWithoutStackTrace (aThrowable));
+      return EHandled.HANDLED;
+    }
+    if (aThrowable instanceof SMPSMLException)
+    {
+      if (SMPServerConfiguration.isRESTLogExceptions ())
+        LOGGER.error ("SMP SML error", aThrowable);
+      setSimpleTextResponse (aUnifiedResponse,
+                             HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                             GlobalDebug.isDebugMode () ? getResponseEntityWithStackTrace (aThrowable)
+                                                        : getResponseEntityWithoutStackTrace (aThrowable));
+      return EHandled.HANDLED;
+    }
+    if (aThrowable instanceof SMPNotFoundException)
+    {
+      if (SMPServerConfiguration.isRESTLogExceptions ())
+        LOGGER.error ("Not found", aThrowable);
+      setSimpleTextResponse (aUnifiedResponse,
+                             HttpServletResponse.SC_NOT_FOUND,
+                             getResponseEntityWithoutStackTrace (aThrowable));
+      return EHandled.HANDLED;
+    }
+    if (aThrowable instanceof SMPInternalErrorException)
+    {
+      if (SMPServerConfiguration.isRESTLogExceptions ())
+        LOGGER.error ("Internal error", aThrowable);
+      setSimpleTextResponse (aUnifiedResponse,
+                             HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                             GlobalDebug.isDebugMode () ? getResponseEntityWithStackTrace (aThrowable)
+                                                        : getResponseEntityWithoutStackTrace (aThrowable));
+      return EHandled.HANDLED;
+    }
+    if (aThrowable instanceof SMPBadRequestException)
+    {
+      if (SMPServerConfiguration.isRESTLogExceptions ())
+        LOGGER.error ("Bad request", aThrowable);
+      setSimpleTextResponse (aUnifiedResponse,
+                             HttpServletResponse.SC_BAD_REQUEST,
+                             getResponseEntityWithoutStackTrace (aThrowable));
+      return EHandled.HANDLED;
+    }
+    if (aThrowable instanceof SMPServerException)
+    {
+      // Generic fallback only
+      if (SMPServerConfiguration.isRESTLogExceptions ())
+        LOGGER.error ("Generic SMP server", aThrowable);
+      setSimpleTextResponse (aUnifiedResponse,
+                             HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                             getResponseEntityWithoutStackTrace (aThrowable));
+      return EHandled.HANDLED;
+    }
+    if (aThrowable instanceof RuntimeException)
+    {
+      if (SMPServerConfiguration.isRESTLogExceptions ())
+        LOGGER.error ("Runtime exception - " + aThrowable.getClass ().getName (), aThrowable);
+      setSimpleTextResponse (aUnifiedResponse,
+                             HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                             GlobalDebug.isDebugMode () ? getResponseEntityWithStackTrace (aThrowable)
+                                                        : getResponseEntityWithoutStackTrace (aThrowable));
+      return EHandled.HANDLED;
+    }
+
+    // We don't know that exception
+    return EHandled.UNHANDLED;
+  }
+}
--- peppol-smp-server-webapp/src/main/java/com/helger/peppol/smpserver/rest2/Rest2Filter.java
+++ peppol-smp-server-webapp/src/main/java/com/helger/peppol/smpserver/rest2/Rest2Filter.java
@@ -1,31 +1,35 @@
+/**
+ * Copyright (C) 2014-2019 Philip Helger and contributors
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.helger.peppol.smpserver.rest2;
 
 import java.io.IOException;
 
 import javax.annotation.Nonnull;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.helger.commons.debug.GlobalDebug;
 import com.helger.commons.http.EHttpMethod;
 import com.helger.commons.mime.CMimeType;
 import com.helger.commons.state.EContinue;
-import com.helger.commons.state.EHandled;
-import com.helger.peppol.smpserver.SMPServerConfiguration;
-import com.helger.peppol.smpserver.exception.SMPBadRequestException;
-import com.helger.peppol.smpserver.exception.SMPInternalErrorException;
-import com.helger.peppol.smpserver.exception.SMPNotFoundException;
-import com.helger.peppol.smpserver.exception.SMPSMLException;
-import com.helger.peppol.smpserver.exception.SMPServerException;
-import com.helger.peppol.smpserver.exception.SMPUnauthorizedException;
-import com.helger.peppol.smpserver.exception.SMPUnknownUserException;
 import com.helger.photon.core.api.APIDescriptor;
 import com.helger.photon.core.api.APIDescriptorList;
 import com.helger.photon.core.api.APIPath;
-import com.helger.photon.core.api.AbstractAPIExceptionMapper;
 import com.helger.photon.core.api.IAPIExceptionMapper;
 import com.helger.photon.core.api.InvokableAPIDescriptor;
 import com.helger.servlet.response.UnifiedResponse;
@@ -49,95 +53,7 @@ public class Rest2Filter extends AbstractXFilterUnifiedResponse
 
   public Rest2Filter ()
   {
-    final IAPIExceptionMapper aExceptionMapper = new AbstractAPIExceptionMapper ()
-    {
-      @Nonnull
-      public EHandled applyExceptionOnResponse (final InvokableAPIDescriptor aInvokableDescriptor,
-                                                final IRequestWebScopeWithoutResponse aRequestScope,
-                                                final UnifiedResponse aUnifiedResponse,
-                                                final Throwable aThrowable)
-      {
-        // From specific to general
-        if (aThrowable instanceof SMPUnauthorizedException)
-        {
-          if (SMPServerConfiguration.isRESTLogExceptions ())
-            LOGGER.error ("Unauthorized", aThrowable);
-          setSimpleTextResponse (aUnifiedResponse,
-                                 HttpServletResponse.SC_FORBIDDEN,
-                                 getResponseEntityWithoutStackTrace (aThrowable));
-          return EHandled.HANDLED;
-        }
-        if (aThrowable instanceof SMPUnknownUserException)
-        {
-          if (SMPServerConfiguration.isRESTLogExceptions ())
-            LOGGER.error ("Unknown user", aThrowable);
-          setSimpleTextResponse (aUnifiedResponse,
-                                 HttpServletResponse.SC_FORBIDDEN,
-                                 getResponseEntityWithoutStackTrace (aThrowable));
-          return EHandled.HANDLED;
-        }
-        if (aThrowable instanceof SMPSMLException)
-        {
-          if (SMPServerConfiguration.isRESTLogExceptions ())
-            LOGGER.error ("SMP SML error", aThrowable);
-          setSimpleTextResponse (aUnifiedResponse,
-                                 HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
-                                 GlobalDebug.isDebugMode () ? getResponseEntityWithStackTrace (aThrowable)
-                                                            : getResponseEntityWithoutStackTrace (aThrowable));
-          return EHandled.HANDLED;
-        }
-        if (aThrowable instanceof SMPNotFoundException)
-        {
-          if (SMPServerConfiguration.isRESTLogExceptions ())
-            LOGGER.error ("Not found", aThrowable);
-          setSimpleTextResponse (aUnifiedResponse,
-                                 HttpServletResponse.SC_NOT_FOUND,
-                                 getResponseEntityWithoutStackTrace (aThrowable));
-          return EHandled.HANDLED;
-        }
-        if (aThrowable instanceof SMPInternalErrorException)
-        {
-          if (SMPServerConfiguration.isRESTLogExceptions ())
-            LOGGER.error ("Internal error", aThrowable);
-          setSimpleTextResponse (aUnifiedResponse,
-                                 HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
-                                 GlobalDebug.isDebugMode () ? getResponseEntityWithStackTrace (aThrowable)
-                                                            : getResponseEntityWithoutStackTrace (aThrowable));
-          return EHandled.HANDLED;
-        }
-        if (aThrowable instanceof SMPBadRequestException)
-        {
-          if (SMPServerConfiguration.isRESTLogExceptions ())
-            LOGGER.error ("Bad request", aThrowable);
-          setSimpleTextResponse (aUnifiedResponse,
-                                 HttpServletResponse.SC_BAD_REQUEST,
-                                 getResponseEntityWithoutStackTrace (aThrowable));
-          return EHandled.HANDLED;
-        }
-        if (aThrowable instanceof SMPServerException)
-        {
-          // Generic fallback only
-          if (SMPServerConfiguration.isRESTLogExceptions ())
-            LOGGER.error ("Generic SMP server", aThrowable);
-          setSimpleTextResponse (aUnifiedResponse,
-                                 HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
-                                 getResponseEntityWithoutStackTrace (aThrowable));
-          return EHandled.HANDLED;
-        }
-        if (aThrowable instanceof RuntimeException)
-        {
-          if (SMPServerConfiguration.isRESTLogExceptions ())
-            LOGGER.error ("Runtime exception - " + aThrowable.getClass ().getName (), aThrowable);
-          setSimpleTextResponse (aUnifiedResponse,
-                                 HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
-                                 GlobalDebug.isDebugMode () ? getResponseEntityWithStackTrace (aThrowable)
-                                                            : getResponseEntityWithoutStackTrace (aThrowable));
-          return EHandled.HANDLED;
-        }
-
-        return EHandled.UNHANDLED;
-      }
-    };
+    final IAPIExceptionMapper aExceptionMapper = new Rest2ExceptionMapper ();
 
     // BusinessCard
     {
