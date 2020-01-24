cad2820c834a899d3cb44b09891d1bf7512de7e8
Philip Helger <philip@helger.com>
2020-01-24 22:55:46 +0100

    Added Linus release script

---
+++ docker/build-release-latest.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+version=5.2.1
+
+# --------------- Building -----------------------
+
+docker pull tomcat:9-jre11
+
+# --------------- XML -----------------------
+
+docker build --build-arg SMP_VERSION=$version -t phoss-smp-release-binary-xml-$version -f Dockerfile-release-binary-xml .
+
+# legacy names
+
+docker tag phoss-smp-release-binary-xml-$version phelger/smp:$version
+docker tag phoss-smp-release-binary-xml-$version phelger/smp:latest
+
+# new names
+
+docker tag phoss-smp-release-binary-xml-$version phelger/phoss-smp-xml:$version
+docker tag phoss-smp-release-binary-xml-$version phelger/phoss-smp-xml:latest
+
+# --------------- SQL -----------------------
+
+docker build --build-arg SMP_VERSION=$version -t phoss-smp-release-binary-sql-$version -f Dockerfile-release-binary-sql .
+docker tag phoss-smp-release-binary-sql-$version phelger/phoss-smp-sql:$version
+docker tag phoss-smp-release-binary-sql-$version phelger/phoss-smp-sql:latest
+
+# --------------- MongoDB -----------------------
+
+docker build --build-arg SMP_VERSION=$version -t phoss-smp-release-binary-mongodb-$version -f Dockerfile-release-binary-mongodb .
+docker tag phoss-smp-release-binary-mongodb-$version phelger/phoss-smp-mongodb:$version
+docker tag phoss-smp-release-binary-mongodb-$version phelger/phoss-smp-mongodb:latest
+
+# --------------- Pushing -----------------------
+
+docker login --username phelger
+
+# --------------- XML -----------------------
+
+docker push phelger/smp:$version
+docker push phelger/smp:latest
+docker push phelger/phoss-smp-xml:$version
+docker push phelger/phoss-smp-xml:latest
+
+# --------------- SQL -----------------------
+
+docker push phelger/phoss-smp-sql:$version
+docker push phelger/phoss-smp-sql:latest
+
+# --------------- MongoDB -----------------------
+
+docker push phelger/phoss-smp-mongodb:$version
+docker push phelger/phoss-smp-mongodb:latest
+
+# --------------- finalize
+
+docker logout
+
