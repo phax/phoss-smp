056bdf0f0a4f7ffbb7dfc415c6b59f75d8906793
Philip Helger <philip@helger.com>
2020-08-20 23:06:08 +0200

    Updated license headers

--- phoss-smp-backend-sql/database_backups/smp_postgresql_20200820_5.2.x.sql
+++ phoss-smp-backend-sql/database_backups/smp_postgresql_20200820_5.2.x.sql
@@ -1,3 +1,14 @@
+--
+-- Copyright (C) 2015-2020 Philip Helger and contributors
+-- philip[at]helger[dot]com
+--
+-- The Original Code is Copyright The Peppol project (http://www.peppol.eu)
+--
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/.
+--
+
 CREATE DATABASE smp;
 
 CREATE TABLE smp_user (
