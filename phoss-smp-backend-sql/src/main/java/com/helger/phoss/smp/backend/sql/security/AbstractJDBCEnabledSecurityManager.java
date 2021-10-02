914f5a26bfca63db06823369bbe9c01abf2b12b8
Philip Helger <philip@helger.com>
2021-10-03 00:17:10 +0200

    Cleansing

--- phoss-smp-backend-sql/src/main/java/com/helger/phoss/smp/backend/sql/security/AbstractJDBCEnabledSecurityManager.java
+++ phoss-smp-backend-sql/src/main/java/com/helger/phoss/smp/backend/sql/security/AbstractJDBCEnabledSecurityManager.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright (C) 2019-2021 Philip Helger and contributors
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.helger.phoss.smp.backend.sql.security;
 
 import java.util.Map;
@@ -6,8 +22,11 @@ import java.util.function.Supplier;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
+import com.helger.commons.annotation.ReturnsMutableCopy;
 import com.helger.commons.collection.CollectionHelper;
+import com.helger.commons.collection.impl.CommonsArrayList;
 import com.helger.commons.collection.impl.CommonsLinkedHashMap;
+import com.helger.commons.collection.impl.ICommonsList;
 import com.helger.commons.collection.impl.ICommonsOrderedMap;
 import com.helger.commons.string.StringHelper;
 import com.helger.db.jdbc.executor.DBExecutor;
@@ -27,6 +46,13 @@ public abstract class AbstractJDBCEnabledSecurityManager extends AbstractJDBCEna
   protected AbstractJDBCEnabledSecurityManager (@Nonnull final Supplier <? extends DBExecutor> aDBExecSupplier)
   {
     super (aDBExecSupplier);
+  }
+
+  @Nonnull
+  @ReturnsMutableCopy
+  public <T> ICommonsList <T> getNone ()
+  {
+    return new CommonsArrayList <> ();
   }
 
   @Nullable
