42e883a431484e871652fe157ff1aaf7e3e25e3e
Philip Helger <philip@helger.com>
2021-10-02 22:51:55 +0200

    Beautified

--- phoss-smp-backend-sql/src/main/resources/db/migrate-postgresql/V1__InitDatabase-5.2-PostgreSQL.sql
+++ phoss-smp-backend-sql/src/main/resources/db/migrate-postgresql/V1__InitDatabase-5.2-PostgreSQL.sql
@@ -25,87 +25,87 @@ INSERT INTO smp_user VALUES ('peppol_user','Test1234');
 
 CREATE TABLE smp_service_group (
   businessIdentifierScheme varchar(25) NOT NULL,
-  businessIdentifier varchar(50) NOT NULL,
-  extension text,
+  businessIdentifier       varchar(50) NOT NULL,
+  extension                text,
   PRIMARY KEY (businessIdentifierScheme,businessIdentifier)
 );
 
 CREATE TABLE smp_service_metadata (
-  businessIdentifierScheme varchar(25) NOT NULL,
-  businessIdentifier varchar(50) NOT NULL,
-  documentIdentifierScheme varchar(25) NOT NULL,
-  documentIdentifier varchar(500) NOT NULL,
-  extension text,
+  businessIdentifierScheme varchar(25)  NOT NULL,
+  businessIdentifier       varchar(50)  NOT NULL,
+  documentIdentifierScheme varchar(25)  NOT NULL,
+  documentIdentifier       varchar(500) NOT NULL,
+  extension                text,
   PRIMARY KEY (businessIdentifierScheme,businessIdentifier,documentIdentifierScheme,documentIdentifier),
   CONSTRAINT FK_smp_service_metadata_businessIdentifier FOREIGN KEY (businessIdentifierScheme, businessIdentifier) REFERENCES smp_service_group (businessIdentifierScheme, businessIdentifier) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE smp_process (
-  businessIdentifierScheme varchar(25) NOT NULL,
-  businessIdentifier varchar(50) NOT NULL,
-  documentIdentifierScheme varchar(25) NOT NULL,
-  documentIdentifier varchar(500) NOT NULL,
-  processIdentifierType varchar(25) NOT NULL,
-  processIdentifier varchar(200) NOT NULL,
-  extension text,
+  businessIdentifierScheme varchar(25)  NOT NULL,
+  businessIdentifier       varchar(50)  NOT NULL,
+  documentIdentifierScheme varchar(25)  NOT NULL,
+  documentIdentifier       varchar(500) NOT NULL,
+  processIdentifierType    varchar(25)  NOT NULL,
+  processIdentifier        varchar(200) NOT NULL,
+  extension                text,
   PRIMARY KEY (businessIdentifierScheme,businessIdentifier,documentIdentifierScheme,documentIdentifier,processIdentifierType,processIdentifier),
   CONSTRAINT FK_smp_process_documentIdentifierScheme FOREIGN KEY (businessIdentifierScheme, businessIdentifier, documentIdentifierScheme, documentIdentifier) REFERENCES smp_service_metadata (businessIdentifierScheme, businessIdentifier, documentIdentifierScheme, documentIdentifier) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE smp_endpoint (
-  businessIdentifierScheme varchar(25) NOT NULL,
-  businessIdentifier varchar(50) NOT NULL,
-  documentIdentifierScheme varchar(25) NOT NULL,
-  documentIdentifier varchar(500) NOT NULL,
-  processIdentifierType varchar(25) NOT NULL,
-  processIdentifier varchar(200) NOT NULL,
-  certificate text NOT NULL,
-  endpointReference varchar(256) NOT NULL,
-  minimumAuthenticationLevel varchar(256) DEFAULT NULL,
-  requireBusinessLevelSignature boolean NOT NULL,
-  serviceActivationDate timestamp DEFAULT NULL,
-  serviceDescription text NOT NULL,
-  serviceExpirationDate timestamp DEFAULT NULL,
-  technicalContactUrl varchar(256) NOT NULL,
-  technicalInformationUrl varchar(256) DEFAULT NULL,
-  transportProfile varchar(256) NOT NULL,
-  extension text,
+  businessIdentifierScheme      varchar(25)  NOT NULL,
+  businessIdentifier            varchar(50)  NOT NULL,
+  documentIdentifierScheme      varchar(25)  NOT NULL,
+  documentIdentifier            varchar(500) NOT NULL,
+  processIdentifierType         varchar(25)  NOT NULL,
+  processIdentifier             varchar(200) NOT NULL,
+  certificate                   text         NOT NULL,
+  endpointReference             varchar(256) NOT NULL,
+  minimumAuthenticationLevel    varchar(256) DEFAULT NULL,
+  requireBusinessLevelSignature boolean      NOT NULL,
+  serviceActivationDate         timestamp    DEFAULT NULL,
+  serviceDescription            text         NOT NULL,
+  serviceExpirationDate         timestamp    DEFAULT NULL,
+  technicalContactUrl           varchar(256) NOT NULL,
+  technicalInformationUrl       varchar(256) DEFAULT NULL,
+  transportProfile              varchar(256) NOT NULL,
+  extension                     text,
   PRIMARY KEY (businessIdentifierScheme,businessIdentifier,documentIdentifierScheme,documentIdentifier,processIdentifierType,processIdentifier,transportProfile),
   CONSTRAINT FK_smp_endpoint_documentIdentifierScheme FOREIGN KEY (businessIdentifierScheme, businessIdentifier, documentIdentifierScheme, documentIdentifier, processIdentifierType, processIdentifier) REFERENCES smp_process (businessIdentifierScheme, businessIdentifier, documentIdentifierScheme, documentIdentifier, processIdentifierType, processIdentifier) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE smp_ownership (
-  businessIdentifierScheme varchar(25) NOT NULL,
-  businessIdentifier varchar(50) NOT NULL,
-  username varchar(256) NOT NULL,
+  businessIdentifierScheme varchar(25)  NOT NULL,
+  businessIdentifier       varchar(50)  NOT NULL,
+  username                 varchar(256) NOT NULL,
   PRIMARY KEY (businessIdentifierScheme,businessIdentifier,username),
   CONSTRAINT FK_smp_ownership_id FOREIGN KEY (businessIdentifierScheme, businessIdentifier) REFERENCES smp_service_group (businessIdentifierScheme, businessIdentifier) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT FK_smp_ownership_username FOREIGN KEY (username) REFERENCES smp_user (username) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE smp_service_metadata_redirection (
-  businessIdentifierScheme varchar(25) NOT NULL,
-  businessIdentifier varchar(50) NOT NULL,
-  documentIdentifierScheme varchar(25) NOT NULL,
-  documentIdentifier varchar(500) NOT NULL,
-  certificateUID varchar(256) DEFAULT NULL,
-  redirectionUrl varchar(256) NOT NULL,
-  extension text,
-  certificate text,
+  businessIdentifierScheme varchar(25)  NOT NULL,
+  businessIdentifier       varchar(50)  NOT NULL,
+  documentIdentifierScheme varchar(25)  NOT NULL,
+  documentIdentifier       varchar(500) NOT NULL,
+  certificateUID           varchar(256) DEFAULT NULL,
+  redirectionUrl           varchar(256) NOT NULL,
+  extension                text,
+  certificate              text,
   PRIMARY KEY (documentIdentifierScheme,businessIdentifier,businessIdentifierScheme,documentIdentifier),
   CONSTRAINT FK_smp_redirect_businessIdentifier FOREIGN KEY (businessIdentifierScheme, businessIdentifier) REFERENCES smp_service_group (businessIdentifierScheme, businessIdentifier) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE smp_bce (
-  id varchar(45) NOT NULL,
-  pid varchar(255) NOT NULL,
-  name text NOT NULL,
-  country varchar(3) NOT NULL,
-  geoinfo text,
+  id          varchar(45)  NOT NULL,
+  pid         varchar(255) NOT NULL,
+  name        text         NOT NULL,
+  country     varchar(3)   NOT NULL,
+  geoinfo     text,
   identifiers text,
-  websites text,
-  contacts text,
-  addon text,
-  regdate date DEFAULT NULL,
+  websites    text,
+  contacts    text,
+  addon       text,
+  regdate     date DEFAULT NULL,
   PRIMARY KEY (id)
 );
