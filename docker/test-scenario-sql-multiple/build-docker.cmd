481a251c1e936ca6e30d8b34261b09ae23f88bae
Philip Helger <philip@helger.com>
2022-01-03 20:32:39 +0100

    Playground

---
+++ docker/test-scenario-sql-multiple/build-docker.cmd
@@ -0,0 +1,28 @@
+@REM
+@REM Copyright (C) 2015-2022 Philip Helger and contributors
+@REM philip[at]helger[dot]com
+@REM
+@REM Licensed under the Apache License, Version 2.0 (the "License");
+@REM you may not use this file except in compliance with the License.
+@REM You may obtain a copy of the License at
+@REM
+@REM         http://www.apache.org/licenses/LICENSE-2.0
+@REM
+@REM Unless required by applicable law or agreed to in writing, software
+@REM distributed under the License is distributed on an "AS IS" BASIS,
+@REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@REM See the License for the specific language governing permissions and
+@REM limitations under the License.
+@REM
+
+@echo off
+
+docker build --pull -t phoss-smp-snapshot-from-binary-sql .
+if errorlevel 1 goto end
+
+echo Tagging
+docker tag phoss-smp-snapshot-from-binary-sql phelger/phoss-smp-sql:snapshot
+goto end
+
+:end
+
