7fff939cd72dfd56c4423ccf9b22c2613b2a4f49
Philip Helger <philip@helger.com>
2022-01-03 21:29:49 +0100

    Fix the post login redirect URL after reverse proxy

--- phoss-smp-webapp/src/main/java/com/helger/phoss/smp/ui/SMPLoginManager.java
+++ phoss-smp-webapp/src/main/java/com/helger/phoss/smp/ui/SMPLoginManager.java
@@ -18,10 +18,14 @@ package com.helger.phoss.smp.ui;
 
 import javax.annotation.Nonnull;
 
+import com.helger.phoss.smp.SMPServerConfiguration;
 import com.helger.phoss.smp.app.CSMP;
 import com.helger.photon.app.html.IHTMLProvider;
 import com.helger.photon.bootstrap4.uictrls.ext.BootstrapLoginManager;
+import com.helger.photon.core.servlet.AbstractSecureApplicationServlet;
 import com.helger.security.authentication.credentials.ICredentialValidationResult;
+import com.helger.servlet.StaticServerInfo;
+import com.helger.web.scope.IRequestWebScopeWithoutResponse;
 
 /**
  * The login manager to be used. Manages login process incl. UI.
@@ -40,5 +44,18 @@ public final class SMPLoginManager extends BootstrapLoginManager
   protected IHTMLProvider createLoginScreen (final boolean bLoginError, @Nonnull final ICredentialValidationResult aLoginResult)
   {
     return new SMPLoginHTMLProvider (bLoginError, aLoginResult, getPageTitle ());
+  }
+
+  @Override
+  protected String getPostLoginRedirectURL (@Nonnull final IRequestWebScopeWithoutResponse aRequestScope)
+  {
+    final boolean bIsForceRoot = SMPServerConfiguration.isForceRoot ();
+    final String ret;
+    if (bIsForceRoot)
+      ret = StaticServerInfo.getInstance ().getFullServerPath ();
+    else
+      ret = StaticServerInfo.getInstance ().getFullContextPath ();
+
+    return ret + AbstractSecureApplicationServlet.SERVLET_DEFAULT_PATH;
   }
 }
