b65b662f681b5babd6495a79b3b8256425a0f65d
Philip Helger <philip@helger.com>
2022-03-27 17:53:29 +0200

    Updated

--- docker/db2/shell.cmd
+++ docker/db2/shell.cmd
@@ -1,3 +1,20 @@
+@REM
+@REM Copyright (C) 2015-2022 Philip Helger and contributors
+@REM philip[at]helger[dot]com
+@REM
+@REM Licensed under the Apache License, Version 2.0 (the "License");
+@REM you may not use this file except in compliance with the License.
+@REM You may obtain a copy of the License at
+@REM
+@REM         http://www.apache.org/licenses/LICENSE-2.0
+@REM
+@REM Unless required by applicable law or agreed to in writing, software
+@REM distributed under the License is distributed on an "AS IS" BASIS,
+@REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@REM See the License for the specific language governing permissions and
+@REM limitations under the License.
+@REM
+
 @echo off
 ::docker exec -ti mydb2 bash -c "su - ${DB2INSTANCE}"
 docker exec -ti mydb2 bash -c "su - db2inst1"
--- phoss-smp-backend/src/main/java/com/helger/phoss/smp/ESMPRESTType.java
+++ phoss-smp-backend/src/main/java/com/helger/phoss/smp/ESMPRESTType.java
@@ -14,6 +14,7 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import com.helger.commons.annotation.Nonempty;
+import com.helger.commons.annotation.Since;
 import com.helger.commons.id.IHasID;
 import com.helger.commons.lang.EnumHelper;
 import com.helger.commons.name.IHasDisplayName;
@@ -31,7 +32,8 @@ public enum ESMPRESTType implements IHasID <String>, IHasDisplayName
 {
   PEPPOL ("peppol", "Peppol", ESMPAPIType.PEPPOL),
   OASIS_BDXR_V1 ("bdxr", "OASIS BDXR SMP v1", ESMPAPIType.OASIS_BDXR_V1),
-  OASIS_BDXR_V2 ("bdxr2", "OASIS BDXR SMP v2", ESMPAPIType.OASIS_BDXR_V2);
+  @Since ("5.7.0")
+  OASIS_BDXR_V2("bdxr2", "OASIS BDXR SMP v2", ESMPAPIType.OASIS_BDXR_V2);
 
   private final String m_sID;
   private final String m_sDisplayName;
