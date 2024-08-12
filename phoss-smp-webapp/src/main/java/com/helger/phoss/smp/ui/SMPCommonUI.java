f59d9ea550b5fe4d3698ce9ca4a7c17ce5c72943
Philip Helger <philip@helger.com>
2024-08-12 22:09:20 +0200

    Replaced deprecated API

--- phoss-smp-webapp/src/main/java/com/helger/phoss/smp/ui/SMPCommonUI.java
+++ phoss-smp-webapp/src/main/java/com/helger/phoss/smp/ui/SMPCommonUI.java
@@ -119,8 +119,7 @@ public final class SMPCommonUI
                                                                                           aTable.getID ())))
                  .setServerFilterType (EDataTablesFilterType.ALL_TERMS_PER_ROW)
                  .setTextLoadingURL (CAjax.DATATABLES_I18N.getInvocationURL (aRequestScope),
-                                     AjaxExecutorDataTablesI18N.REQUEST_PARAM_LANGUAGE_ID,
-                                     AjaxExecutorDataTablesI18N.REQUEST_PARAM_MAX_PAGES)
+                                     AjaxExecutorDataTablesI18N.REQUEST_PARAM_LANGUAGE_ID)
                  .addPlugin (new DataTablesPluginSearchHighlight ());
     });
 
