da25c81f7a98a964ea06f50e28ba51af17d4ff4f
Philip Helger <philip@helger.com>
2025-02-27 13:37:48 +0100

    Updated for 7.2.1

--- docker/build-all.cmd
+++ docker/build-all.cmd
@@ -17,7 +17,7 @@
 
 @echo off
 
-set version=7.2.0
+set version=7.2.1
 
 echo Docker login
 docker login --username phelger
--- docker/build-all.sh
+++ docker/build-all.sh
@@ -16,7 +16,7 @@
 # limitations under the License.
 #
 
-version=7.2.0
+version=7.2.1
 
 echo Docker login
 docker login --username phelger
