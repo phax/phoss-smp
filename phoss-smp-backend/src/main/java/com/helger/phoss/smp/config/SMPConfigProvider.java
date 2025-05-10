665100dcdef6eda08c0e7ee9b2f5190f6c7cfc0f
Philip Helger <philip@helger.com>
2025-05-10 23:28:09 +0200

    Finally removed support for the old configuration files

--- phoss-smp-backend/src/main/java/com/helger/phoss/smp/config/SMPConfigProvider.java
+++ phoss-smp-backend/src/main/java/com/helger/phoss/smp/config/SMPConfigProvider.java
@@ -10,8 +10,6 @@
  */
 package com.helger.phoss.smp.config;
 
-import java.nio.charset.StandardCharsets;
-
 import javax.annotation.Nonnull;
 import javax.annotation.concurrent.GuardedBy;
 import javax.annotation.concurrent.ThreadSafe;
@@ -24,15 +22,12 @@ import com.helger.commons.concurrent.SimpleReadWriteLock;
 import com.helger.commons.debug.GlobalDebug;
 import com.helger.commons.equals.EqualsHelper;
 import com.helger.commons.exception.InitializationException;
-import com.helger.commons.io.resource.IReadableResource;
-import com.helger.commons.io.resourceprovider.ReadableResourceProviderChain;
 import com.helger.commons.string.StringHelper;
 import com.helger.commons.system.SystemProperties;
 import com.helger.config.ConfigFactory;
 import com.helger.config.IConfig;
 import com.helger.config.fallback.IConfigWithFallback;
 import com.helger.config.source.MultiConfigurationValueProvider;
-import com.helger.config.source.res.ConfigurationSourceProperties;
 
 /**
  * The global configuration provider for SMP V6.
@@ -84,8 +79,8 @@ public final class SMPConfigProvider
   }
 
   /**
-   * @return The configuration value provider for SMP client that contains
-   *         backward compatibility support.
+   * @return The configuration value provider for SMP client that contains backward compatibility
+   *         support.
    */
   @Nonnull
   public static MultiConfigurationValueProvider createSMPClientValueProvider ()
@@ -93,39 +88,6 @@ public final class SMPConfigProvider
     // Start with default setup
     final MultiConfigurationValueProvider ret = ConfigFactory.createDefaultValueProvider ();
 
-    final ReadableResourceProviderChain aResourceProvider = ConfigFactory.createDefaultResourceProviderChain ();
-
-    IReadableResource aRes;
-    final int nBasePrio = ConfigFactory.APPLICATION_PROPERTIES_PRIORITY;
-
-    final String sDefaultSuffix = "\n  Place all properties in 'application.properties' instead. !This fallback will be removed in version 7 of phoss SMP!";
-
-    // TODO remove block in v7
-    // Lower priority than the standard files
-    aRes = aResourceProvider.getReadableResourceIf ("private-smp-server.properties", IReadableResource::exists);
-    if (aRes != null)
-    {
-      LOGGER.warn ("The support for the properties file '" + aRes.getAsURL () + "' is deprecated." + sDefaultSuffix);
-      ret.addConfigurationSource (new ConfigurationSourceProperties (aRes, StandardCharsets.UTF_8), nBasePrio - 1);
-    }
-    aRes = aResourceProvider.getReadableResourceIf ("smp-server.properties", IReadableResource::exists);
-    if (aRes != null)
-    {
-      LOGGER.warn ("The support for the properties file '" + aRes.getAsURL () + "' is deprecated." + sDefaultSuffix);
-      ret.addConfigurationSource (new ConfigurationSourceProperties (aRes, StandardCharsets.UTF_8), nBasePrio - 2);
-    }
-    aRes = aResourceProvider.getReadableResourceIf ("private-webapp.properties", IReadableResource::exists);
-    if (aRes != null)
-    {
-      LOGGER.warn ("The support for the properties file '" + aRes.getAsURL () + "' is deprecated." + sDefaultSuffix);
-      ret.addConfigurationSource (new ConfigurationSourceProperties (aRes, StandardCharsets.UTF_8), nBasePrio - 1);
-    }
-    aRes = aResourceProvider.getReadableResourceIf ("webapp.properties", IReadableResource::exists);
-    if (aRes != null)
-    {
-      LOGGER.warn ("The support for the properties file '" + aRes.getAsURL () + "' is deprecated." + sDefaultSuffix);
-      ret.addConfigurationSource (new ConfigurationSourceProperties (aRes, StandardCharsets.UTF_8), nBasePrio - 2);
-    }
     return ret;
   }
 
