712fceca3965b85dc135708eff7064a7cd7bddd8
Philip Helger <philip@helger.com>
2026-01-25 21:03:34 +0100

    Updated

--- phoss-smp-backend/src/test/java/com/helger/phoss/smp/domain/sgprops/SGCustomPropertyListTest.java
+++ phoss-smp-backend/src/test/java/com/helger/phoss/smp/domain/sgprops/SGCustomPropertyListTest.java
@@ -1,6 +1,19 @@
+/*
+ * Copyright (C) 2015-2025 Philip Helger and contributors
+ * philip[at]helger[dot]com
+ *
+ * The Original Code is Copyright The Peppol project (http://www.peppol.eu)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.helger.phoss.smp.domain.sgprops;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -18,8 +31,21 @@ public final class SGCustomPropertyListTest
   public void testBasic ()
   {
     final SGCustomPropertyList aList = new SGCustomPropertyList ();
-    aList.add (SGCustomProperty.createPrivate ("key1", "value1"));
-    aList.add (SGCustomProperty.createPublic ("key2", "value2"));
+    assertTrue (aList.isEmpty ());
+    assertEquals (0, aList.size ());
+
+    assertTrue (aList.add (SGCustomProperty.createPrivate ("key1", "value1")).isChanged ());
+    assertFalse (aList.isEmpty ());
+    assertEquals (1, aList.size ());
+
+    assertTrue (aList.add (SGCustomProperty.createPublic ("key2", "value2")).isChanged ());
+    assertFalse (aList.isEmpty ());
+    assertEquals (2, aList.size ());
+
+    assertFalse (aList.add (SGCustomProperty.createPublic ("key2", "value2a")).isChanged ());
+    assertFalse (aList.isEmpty ());
+    assertEquals (2, aList.size ());
+
     final IJsonArray aJson = aList.getAsJson ();
     assertNotNull (aJson);
     final SGCustomPropertyList aList2 = SGCustomPropertyList.fromJson (aJson);
